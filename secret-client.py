0a2e12f88bc21674f6536369ff921e3ee87a4c42
David Hernández Martín <17512929+belcebus@users.noreply.github.com>
2023-11-07 22:29:29 +0000

    translate to english

--- secret-client.py
+++ secret-client.py
@@ -6,18 +6,18 @@ import nacl.signing
 import nacl.secret
 import argparse
 
-# Crea el parser
-parser = argparse.ArgumentParser(description="Script para añadir un secreto a un repositorio de GitHub.")
+# Create the parser
+parser = argparse.ArgumentParser(description="Script to add a secret to a GitHub repository.")
 
-# Agrega los argumentos
-parser.add_argument('--name', type=str, required=True, help='Nombre del secreto', metavar='SECRET_NAME')
-parser.add_argument('--value', type=str, required=True, help='Valor del secreto', metavar='SECRET_VALUE')
-parser.add_argument('--org', type=str, required=True, help='Nombre de la organización o usuario')
-parser.add_argument('--repo', type=str, required=True, help='Nombre del repositorio')
+# Add the arguments
+parser.add_argument('--name', type=str, required=True, help='Name of the secret', metavar='SECRET_NAME')
+parser.add_argument('--value', type=str, required=True, help='Value of the secret', metavar='SECRET_VALUE')
+parser.add_argument('--org', type=str, required=True, help='Name of the organization or user')
+parser.add_argument('--repo', type=str, required=True, help='Name of the repository')
 parser.add_argument('--token', type=str, required=True, help='Token')
-parser.add_argument('--env', type=str, required=False, help='Environment del repositorio al que asociar el secreto')
+parser.add_argument('--env', type=str, required=False, help='Repository environment to associate the secret with')
 
-# Parsea los argumentos
+# Parse the arguments
 args = parser.parse_args()
 
 
@@ -27,9 +27,9 @@ owner_name = args.owner_name
 repo_name = args.repo_name
 token = args.token
 
-# Encriptar el secreto con la clave pública del repositorio usando libsodium
+# Encrypt the secret with the repository's public key using libsodium
 
-# Obtener la clave pública del repositorio
+# Get the repository's public key
 url = f"https://api.github.com/repos/{owner_name}/{repo_name}/actions/secrets/public-key"
 headers = {
     "Accept": "application/vnd.github.v3+json",
@@ -37,7 +37,7 @@ headers = {
 }
 print("URL: ", url)
 print("Headers: ", headers)
-# Enviar la petición GET para obtener la clave pública
+# Send the GET request to get the public key
 response = requests.get(url, headers=headers)
 
 public_key = response.json()["key"]
@@ -49,19 +49,19 @@ print("public_key_id: ", public_key_id)
 # Decode the public key from base64
 public_key_bytes = base64.b64decode(public_key)
 
-# Encriptar el secreto con la clave pública del repositorio usando libsodium
+# Encrypt the secret with the repository's public key using libsodium
 
 sealed_box = nacl.public.SealedBox(nacl.public.PublicKey(public_key_bytes))
 encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
 
 print("Encrypted: ", encrypted)
 
-# Añadir el secreto a GitHub
+# Add the secret to GitHub
 
-# Definir la URL de la API de GitHub para añadir un secreto
+# Define the GitHub API URL to add a secret
 url = f"https://api.github.com/repos/{owner_name}/{repo_name}/actions/secrets/{secret_name}"
 
-# transformar el secreto en base64 en un objeto serializable para poder enviarlo en la peticion
+# Transform the base64-encoded secret into a serializable object to be able to send it in the request
 encrypted_value = base64.b64encode(encrypted).decode("utf-8")
 data = {
     "encrypted_value": encrypted_value,
@@ -76,16 +76,16 @@ print("URL: ", url)
 print("Data: ", data)
 print("Headers: ", headers)
 
-# Enviar la petición POST para añadir el secreto
+# Send the PUT request to add the secret
 response = requests.put(url, headers=headers, data=json.dumps(data))
 
 print("Response: ", response)   
 
-# Comprobar si la petición ha sido exitosa con un código 201 para un nuevo secreto o
-# 204 para la actualización de un secreto existente
+# Check if the request was successful with a 201 code for a new secret or
+# 204 for an existing secret update
 if response.status_code == 201:
-    print("El secreto ha sido añadido correctamente.")
+    print("The secret has been added successfully.")
 elif response.status_code == 204:
-    print("El secreto ha sido actualizado correctamente.")
+    print("The secret has been updated successfully.")
 else:
-    print("Ha ocurrido un error al añadir el secreto.")
+    print("An error occurred while adding the secret.")
